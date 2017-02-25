620aef40e153c5cdfd68d7628aa9ad90d3233b0e
kirzuev <kir2207@list.ru>
2017-02-25 19:36:43 +0300

    Update main.cpp

--- Zuev/tests/main.cpp
+++ Zuev/tests/main.cpp
@@ -1,23 +1,4 @@
-#include <iostream>
-
 #include "stdafx.h"
-
-using namespace std;
-
-class MyTesting : public ::testing::Test
-{
-		static Translator t;
-		static void SetUpTestCase()  
-		{
-			cout << "Start" << endl;
-		}
-		static void TearDownTestCase()
-		{
-			cout << "Finish" << endl;
-		}
-		void SetUp() override {} 
-		void TearDown() override {}
-};
 
 int main(int argc, char* argv[])
 {
