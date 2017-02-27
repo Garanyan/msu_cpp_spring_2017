ab7b0fc38633796684c141271840f238e3efb89e
konstantin <kos5tya@yandex.ru>
2017-02-28 00:38:29 +0300

    done

--- Bespalov/src/main.cpp
+++ Bespalov/src/main.cpp
@@ -1,6 +1,12 @@
 #include "stdafx.h"
+#include "t9sol.h"
+#include <iostream>
+#include <string>
 
 int main(int argc, char* argv[])
 {
+	std::string str;
+	std::getline(std::cin, str);
+	std::cout << t9solve(str) << std::endl;
     return 0;
 }
