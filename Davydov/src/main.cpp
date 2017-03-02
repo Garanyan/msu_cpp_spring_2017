001a603f94e102de808ce8c66d9aa6b766c412b0
Давыдов Денис <dennic@inbox.ru>
2017-03-02 22:53:01 +0300

    First init

---
+++ Davydov/src/main.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include <map>
+#include <string>
+using std::string;
+
+const std::map<int, string> keypad =
+{
+	{ 2, "abc" },
+	{ 3, "def" },
+	{ 4, "ghi" },
+	{ 5, "jkl" },
+	{ 6, "mno" },
+	{ 7, "pqrs" },
+	{ 8, "tuv" },
+	{ 9, "wxyz" }
+};
+
+class Foo {
+
+};
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
