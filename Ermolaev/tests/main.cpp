5c05c483b320aeb2c5cdc21049440f27a8eef46a
ErmolaevPA <ermolaev.p.a@yandex.ru>
2017-03-03 19:58:41 +0300

    task complete

--- Ermolaev/tests/main.cpp
+++ Ermolaev/tests/main.cpp
@@ -1,4 +1,66 @@
 #include "stdafx.h"
+#include "../src/T9.h"
+
+
+class MyTest : public ::testing::Test
+{
+public:
+	T9 t;
+};
+
+TEST_F(MyTest, test1)
+{
+	EXPECT_STREQ("0", t.translate(" ").c_str());
+    
+	EXPECT_STREQ("2", t.translate("a").c_str());
+	EXPECT_STREQ("22", t.translate("b").c_str());
+	EXPECT_STREQ("222", t.translate("c").c_str());
+
+	EXPECT_STREQ("3", t.translate("d").c_str());
+	EXPECT_STREQ("33", t.translate("e").c_str());
+	EXPECT_STREQ("333", t.translate("f").c_str());
+
+	EXPECT_STREQ("4", t.translate("g").c_str());
+	EXPECT_STREQ("44", t.translate("h").c_str());
+	EXPECT_STREQ("444", t.translate("i").c_str());
+
+	EXPECT_STREQ("5", t.translate("j").c_str());
+	EXPECT_STREQ("55", t.translate("k").c_str());
+	EXPECT_STREQ("555", t.translate("l").c_str());
+
+	EXPECT_STREQ("6", t.translate("m").c_str());
+	EXPECT_STREQ("66", t.translate("n").c_str());
+	EXPECT_STREQ("666", t.translate("o").c_str());
+
+	EXPECT_STREQ("7", t.translate("p").c_str());
+	EXPECT_STREQ("77", t.translate("q").c_str());
+	EXPECT_STREQ("777", t.translate("r").c_str());
+	EXPECT_STREQ("7777", t.translate("s").c_str());
+
+	EXPECT_STREQ("8", t.translate("t").c_str());
+	EXPECT_STREQ("88", t.translate("u").c_str());
+	EXPECT_STREQ("888", t.translate("v").c_str());
+
+	EXPECT_STREQ("9", t.translate("w").c_str());
+	EXPECT_STREQ("99", t.translate("x").c_str());
+	EXPECT_STREQ("999", t.translate("y").c_str());
+	EXPECT_STREQ("9999", t.translate("z").c_str());
+}
+
+TEST_F(MyTest, test2)
+{
+	EXPECT_STREQ("44 444", t.translate("hi").c_str());
+	EXPECT_STREQ("999337777", t.translate("yes").c_str());
+	EXPECT_STREQ("333666 6660 022 2777", t.translate("foo  bar").c_str());
+	EXPECT_STREQ("4433555 555666096667775553", t.translate("hello world").c_str());
+
+    EXPECT_STREQ("", t.translate("").c_str());
+    EXPECT_STREQ("0 0", t.translate("  ").c_str());
+    EXPECT_STREQ("0 04433555 555666096667775553", t.translate("  hello world").c_str());
+    EXPECT_STREQ("4433555 5556660966677755530 0", t.translate("hello world  ").c_str());
+    EXPECT_STREQ("04433555 5556660 0 0966677755530", t.translate(" hello   world ").c_str());
+}
+
 
 int main(int argc, char* argv[])
 {
