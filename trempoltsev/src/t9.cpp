d6186eca31045e5da761d4d184b20ea6e50bbb10
Maksim Trempoltsev <m.trempoltsev@corp.mail.ru>
2017-03-04 09:04:21 +0300

    t9 implementation

--- trempoltsev/src/t9.cpp
+++ trempoltsev/src/t9.cpp
@@ -4,6 +4,68 @@
 
 std::string T9::spell(const std::string& text)
 {
+    static const char PAUSE = ' ';
+
+    static char const* const T9_SPACE = "0";
+
+    static char const* const T9_BY_CHAR[] =
+    {
+        "2",    // a
+        "22",   // b
+        "222",  // c
+
+        "3",    // d
+        "33",   // e
+        "333",  // f
+
+        "4",    // g
+        "44",   // h
+        "444",  // i
+
+        "5",    // j
+        "55",   // k
+        "555",  // l
+
+        "6",    // m
+        "66",   // n
+        "666",  // o
+
+        "7",    // p
+        "77",   // q
+        "777",  // r
+        "7777", // s
+
+        "8",    // t
+        "88",   // u
+        "888",  // v
+
+        "9",    // w
+        "99",   // x
+        "999",  // y
+        "9999", // z
+    };
+
+    if (text.empty())
+        throw std::invalid_argument(std::string("empty string"));
+
     std::string result;
+
+    for (auto c : text)
+    {
+        const char* sequence = nullptr;
+
+        if (c == ' ')
+            sequence = T9_SPACE;
+        else if (c >= 'a' && c <= 'z')
+            sequence = T9_BY_CHAR[c - 'a'];
+        else
+            throw std::invalid_argument(std::string("invalid char: '") + c + "'");
+
+        if (!result.empty() && result.back() == sequence[0])
+            result += PAUSE;
+
+        result += sequence;
+    }
+
     return result;
 }
--- trempoltsev/tests/t9_test.cpp
+++ trempoltsev/tests/t9_test.cpp
@@ -13,11 +13,11 @@ TEST_F(T9_test, spell_chars)
 {
     EXPECT_STREQ("2", t9.spell("a").c_str());
     EXPECT_STREQ("22", t9.spell("b").c_str());
-    EXPECT_STREQ("22", t9.spell("c").c_str());
+    EXPECT_STREQ("222", t9.spell("c").c_str());
 
     EXPECT_STREQ("3", t9.spell("d").c_str());
     EXPECT_STREQ("33", t9.spell("e").c_str());
-    EXPECT_STREQ("33", t9.spell("f").c_str());
+    EXPECT_STREQ("333", t9.spell("f").c_str());
 
     EXPECT_STREQ("4", t9.spell("g").c_str());
     EXPECT_STREQ("44", t9.spell("h").c_str());
@@ -48,11 +48,17 @@ TEST_F(T9_test, spell_chars)
 
 TEST_F(T9_test, spell_words)
 {
-    EXPECT_STREQ("44 444",
+    EXPECT_STREQ("0 044 444",
+        t9.spell("  hi").c_str());
+
+    EXPECT_STREQ("044 444",
         t9.spell(" hi").c_str());
 
-    EXPECT_STREQ("44 444",
+    EXPECT_STREQ("44 4440",
         t9.spell("hi ").c_str());
+
+    EXPECT_STREQ("44 4440 0",
+        t9.spell("hi  ").c_str());
 
     EXPECT_STREQ("44 444",
         t9.spell("hi").c_str());
