b1a3cb2e0bade6499de55d37765ce1750449123c
Ovanes <garanyanos@gmail.com>
2017-03-05 21:05:42 +0300

    T9 Spelling

---
+++ Garanyan/src/header.h
@@ -0,0 +1,15 @@
+/**
+* @brief The class translates words to t9 notation
+*/
+class T9 final
+{
+public:
+	/**
+	* @brief Translate a text to t9 notation
+	* @param text Text to translate. It contains only a-z and space characters
+	* @return The text in T9 notation. It contains only digits and spaces
+	* @throw std::invalid_argument If source text is empty
+	* @throw std::invalid_argument If source text is contain invalid characters
+	*/
+	std::string spell(const std::string& text);
+};
