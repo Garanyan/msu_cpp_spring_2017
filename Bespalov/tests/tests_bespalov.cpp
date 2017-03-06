565be9f1d9e26eb08e86eecea8e8b5783f36f5ad
konstantin <kos5tya@yandex.ru>
2017-03-06 10:23:32 +0300

    task is done

--- Bespalov/tests/tests_bespalov.cpp
+++ Bespalov/tests/tests_bespalov.cpp
@@ -1,16 +1,24 @@
 #include "../src/t9sol.h"
 #include "stdafx.h"
 
-TEST(T9Test, Test1) {
-    ASSERT_EQ("44 444", t9solve("hi"));
-}
-TEST(T9Test, Test2) {
-    ASSERT_EQ("999337777", t9solve("yes"));
-}
-TEST(T9Test, Test3) {
-    ASSERT_EQ("333666 6660 022 2777", t9solve("foo  bar"));
-}
-TEST(T9Test, Test4) {
-    ASSERT_EQ("4433555 555666096667775553", t9solve("hello world"));
+
+
+TEST(T9Test, Cases) {
+    ASSERT_STREQ("44 444", t9solve("hi").c_str());
+
+    ASSERT_STREQ("999337777", t9solve("yes").c_str());
+
+    ASSERT_STREQ("333666 6660 022 2777", t9solve("foo  bar").c_str());
+
+    ASSERT_STREQ("4433555 555666096667775553", t9solve("hello world").c_str());
 }
 
+TEST(T9Test, Errors)
+{
+	ASSERT_STREQ("input is empty", t9solve("").c_str());
+	ASSERT_STREQ("invalid input", t9solve("1").c_str());
+	ASSERT_STREQ("invalid input", t9solve("a1").c_str());
+	ASSERT_STREQ("invalid input", t9solve("Hi").c_str());
+	ASSERT_STREQ("invalid input", t9solve("hello World").c_str());
+	ASSERT_STREQ("invalid input", t9solve("hello 123").c_str());
+}
