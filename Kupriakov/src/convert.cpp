dce7dbc45a65479a8897321ec783c4efa71e0061
rublag <rublag-ns@yandex.ru>
2017-03-10 01:45:17 +0300

    Adapt for C++03

--- Kupriakov/src/convert.cpp
+++ Kupriakov/src/convert.cpp
@@ -4,6 +4,13 @@
 #include <iostream>
 #include <sstream>
 
+static std::string intToString(int n)
+{
+    std::ostringstream strs;
+    strs << n;
+    return strs.str();
+}
+
 static std::string multiply(std::string s, size_t i)
 {
     std::string r;
@@ -22,28 +29,35 @@ bool Converter::convertNextLine(std::string &line)
     }
     catch(const std::ios_base::failure& e)
     {
-        std::cout << e.code() << ": " << e.what();
+        std::cout << "Error: " << e.what();
         return false;
     }
     std::pair<int, int> prev = keytable[input_line[0]];
-    line += multiply(std::to_string(prev.first), prev.second);
+    line += multiply(intToString(prev.first), prev.second);
     for(size_t i = 1; i < input_line.length(); ++i)
     {
         if(keytable[input_line[i]].first == prev.first)
             line += " ";
         prev = keytable[input_line[i]];
-        line += multiply(std::to_string(prev.first), prev.second);
+        line += multiply(intToString(prev.first), prev.second);
     }
     return true;
 }
 
 void Converter::fillKeytable()
 {
-    for(int key = 0; key < keypad.size(); ++key)
+    for(unsigned key = 0; key < keypad.size(); ++key)
     {
-        for (int i = 0; i < keypad[key].length(); ++i)
+        for (unsigned i = 0; i < keypad[key].length(); ++i)
         {
-            keytable.insert({keypad[key][i], {key, i + 1}});
+            keytable.insert(std::pair<char, std::pair<int, int> > (keypad[key][i], std::pair<int, int> (key, i + 1)));
         }
     }
+}
+
+void Converter::fillKeypad()
+{
+    std::string kp[] = {" ", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    for(int i = 0; i < 10; ++i)
+        keypad.push_back(kp[i]);
 }
