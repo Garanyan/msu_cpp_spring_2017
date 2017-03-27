f90dad812656e6c4c42ac9f623a5e1a066ce3298
Bezu <olusha96@gmail.com>
2017-03-27 11:41:23 +0300

    появление заголовочных файлов

--- Bezsudnova/Game/main.cpp
+++ Bezsudnova/Game/main.cpp
@@ -3,69 +3,17 @@
 
 using namespace std;
 
-
-
-
-
-class  Armor
-{
-public:
-    int defence;
-    int fineForSpeed;
-
-    Armor()
-    {
-        defence=0;
-        fineForSpeed=0;
-    }
-
-};
-
-
-
-class Lats: public Armor
-{
-public:
-    Lats()
-    {
-        defence=0;
-        fineForSpeed=0;
-    }
-
-};
-
-class Chain-mail: public Armor
-{
-public:
-    Chain-mail()
-    {
-        defence=0;
-        fineForSpeed=0;
-    }
-
-};
-
-
-class Weapon
-{
-public:
-    int power;
-};
-
-
-
-
-
-
-
+#include "Armor.h"
+#include "Weapon.h"
+#include "Human.h"
 
 int main()
 {
     string small;
     cin >> small;
-    Lyshic Bob(small);
-    Bob.takenArmor<Lats>();
-
+    Knight Bob(small);
+    //Bob.abilityTakeArmor<Lats>();
+    cout << Bob.skill;
 
     return 0;
 }
