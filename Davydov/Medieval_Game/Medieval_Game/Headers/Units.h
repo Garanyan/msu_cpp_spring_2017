9669dd4ab7dd20f61ff74dba45d3665e402c4a1c
Давыдов Денис <dennic@inbox.ru>
2017-04-17 23:43:30 +0300

    First commit for new version of the game

--- Davydov/Medieval_Game/Medieval_Game/Headers/Units.h
+++ Davydov/Medieval_Game/Medieval_Game/Headers/Units.h
@@ -1,1 +1,16 @@
 #pragma once
+
+struct NPC
+{
+public:
+	std::string NPC_name;
+	const std::string& getName();
+	//std::unique_ptr<Weapon> weapon = std::unique_ptr<Fists>(new Fists());
+	//std::unique_ptr<Armor> armor = std::unique_ptr<WithoutArmor>(new WithoutArmor());
+
+	const /*std::string&*/ void eq_weapon(std::unique_ptr<Weapon> && weapon);
+	const void /*std::string&*/ eq_armor(std::unique_ptr<Armor> && armor);
+	Point getHealth();
+
+	void attack(NPC& attacked);
+};
