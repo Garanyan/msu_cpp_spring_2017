0239731b2468c00fff974c86715926c6f6c859d0
Denis Dorozhkin <d.dorogkin@corp.mail.ru>
2017-05-06 12:21:56 +0300

    good testing coverage

--- Dorozhkin/ThreadSafeDeque/tests/tests.cpp
+++ Dorozhkin/ThreadSafeDeque/tests/tests.cpp
@@ -2,25 +2,39 @@
 #include "../include/ThreadSafeDeque.h"
 
 threadsafe_deque<int> deque;
+threadsafe_deque<int> stressdeque;
+int sum;
 
 void get()
 {
     int x;
-    for (auto i = 0; i < 100; i++) {
+    for (auto i = 0; i < 1000; i++) {
         deque.wait_and_pop(x);
+        sum -= x;
     }
 }
 
 void insert()
 {
-    std::this_thread::sleep_for(std::chrono::milliseconds(50));
-    for (auto i = 0; i < 100; i++) {
+    for (auto i = 0; i < 1000; i++) {
+        sum += i;
+        // std::cout << i << std::endl;
+        // std::this_thread::sleep_for(std::chrono::milliseconds(100));
         if (i % 2) {
             deque.push(i);
         } else {
             auto tmp = i;
             deque.emplace(std::move(tmp));
         }
+    }
+}
+
+void stress()
+{
+    int tmp;
+    for (auto i = 0; i < 1000; i++) {
+        stressdeque.push(i);
+        stressdeque.wait_and_pop(tmp);
     }
 }
 
@@ -41,11 +55,31 @@ TEST(SafeDequeTesting, OneThread) {
 }
 
 TEST(SafeDequeTesting, MultiThread) {
+    sum = 0;
     std::thread insertTread(insert);
     std::thread getTread(get);
 
     insertTread.join();
     getTread.join();
+    EXPECT_EQ(sum, 0);
+    // insertTread.detach();
+    // getTread.detach();
+    // 
+    // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+}
+
+TEST(SafeDequeTesting, StressMultiThread) {
+    try {
+        std::vector<std::future<void>> vf;
+        for (auto i = 0; i < 100; i++) {
+            vf.push_back(std::async(std::launch::async, stress));
+        }
+        for (auto& f : vf) {
+            f.wait();
+        }
+    } catch(...) {
+        EXPECT_FALSE(true);
+    }
 }
 
 
